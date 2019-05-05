633e559b9151909ade1ba0f66e6da32158bf5995
fox <fox@aleph.engineering>
2019-05-05 11:03:13 -0400

    Add documentation to compare version function

--- compare_versions/compare_versions.py
+++ compare_versions/compare_versions.py
@@ -1,4 +1,14 @@
 def compare_versions(version1: str, version2: str) -> int:
+    """Compare 2 version strings
+
+    :param version1: Version string; dot separated numbers; white spaces in between are not considered
+    :param version2: Version string; dot separated numbers; white spaces in between are not considered
+    :return:
+        positive number: If the first version is greater than the second
+        negative number: If the first version is smaller than the second
+        zero: If the versions are equals
+    """
+
     version1, version2 = (map(int, v.split('.')) for v in [version1, version2])
     version1, version2 = zip(*map(lambda p1, p2: (p1 or 0, p2 or 0), version1, version2))
 
