644f4cc4552f9d47af74f90c1924a7367079a9e7
fox <fox@aleph.engineering>
2019-05-05 12:19:21 -0400

    Implement cache node representation

--- cache/utils/cache_node.py
+++ cache/utils/cache_node.py
@@ -1,4 +1,19 @@
+from datetime import datetime
+
+from .cache_types import CacheKey, CacheValue
+
+
 class CacheNode:
-    def __init__(self, key, value):
+    def __init__(self,
+                 key: CacheKey,
+                 value: CacheValue,
+                 expire_at: datetime,
+                 next_node: 'CacheNode' = None):
         self.key = key
         self.value = value
+        self.expire_at = expire_at
+        self.next_node = next_node
+        self.previous_node = None
+    
+    def is_expired(self) -> bool:
+        return self.expire_at <= datetime.now()
