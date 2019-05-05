64d73c4ee66c315e7b2c9224a085f4ffbe7a3f0f
fox <fox@aleph.engineering>
2019-05-05 09:21:41 -0400

    Fix overlap function

--- overlap/overlap.py
+++ overlap/overlap.py
@@ -7,5 +7,9 @@ def is_number_inside_boundaries(number: int, boundaries: Line) -> bool:
     return min(boundaries) <= number <= max(boundaries)
 
 
-def overlap(line1: Line, line2: Line) -> bool:
+def overlap_ordered(line1: Line, line2: Line) -> bool:
     return is_number_inside_boundaries(line1[0], line2) or is_number_inside_boundaries(line1[1], line2)
+
+
+def overlap(line1: Line, line2: Line) -> bool:
+    return overlap_ordered(line1, line2) or overlap_ordered(line2, line1)
