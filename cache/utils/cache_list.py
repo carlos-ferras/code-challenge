39b015c7be578a7999891bfce0b481c96ad568b6
fox <fox@aleph.engineering>
2019-05-05 13:38:18 -0400

    Adjust cache list to the new types

--- cache/utils/cache_list.py
+++ cache/utils/cache_list.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+
+from .cache_node import CacheNode
+from .cache_types import CacheKey, CacheValue
+
+
 class DoublyLinkedListCache:
     def __init__(self):
         self.start_node = None
@@ -5,23 +11,26 @@ class DoublyLinkedListCache:
         self.length = 0
         self._node_references = {}
 
-    def add_node(self, key, value):
+    def add_node(self,
+                 key: CacheKey,
+                 value: CacheValue,
+                 expire_at: datetime) -> CacheNode:
         pass
 
-    def move_node_to_start(self, node):
+    def move_node_to_start(self, node: CacheNode) -> None:
         pass
 
-    def delete_node_by_key(self, key):
+    def delete_node_by_key(self, key: CacheKey) -> None:
         pass
 
-    def delete_start_node(self):
+    def delete_start_node(self) -> None:
         pass
 
-    def delete_last_node(self):
+    def delete_last_node(self) -> None:
         pass
 
-    def get_node_by_key(self, key):
+    def get_node_by_key(self, key: CacheKey) -> CacheNode:
         pass
 
-    def has_node_by_key(self, key):
+    def has_node_by_key(self, key: CacheKey) -> bool:
         pass
