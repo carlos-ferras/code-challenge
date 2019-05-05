03da24c67695edc8a10912445a8dccee7400ac2a
fox <fox@aleph.engineering>
2019-05-05 09:10:14 -0400

    Add function to check if 2 lines overlap

--- overlap/overlap.py
+++ overlap/overlap.py
@@ -5,3 +5,7 @@ Line = Tuple[int, int]
 
 def is_number_inside_boundaries(number: int, boundaries: Line) -> bool:
     return min(boundaries) <= number <= max(boundaries)
+
+
+def overlap(line1: Line, line2: Line) -> bool:
+    return is_number_inside_boundaries(line1[0], line2) or is_number_inside_boundaries(line1[1], line2)
